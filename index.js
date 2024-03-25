615ec2ac7a08547773bc29e687107c7bca6e0fa4
Šárka Flídrová <sarka.flidrova@email.cz>
2024-03-25 19:41:07 +0100

    Add js.

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
+const otazka = document.querySelector(".question");
+otazka.textContent = "Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon."
 
+const odpoved = prompt("Odpověď:");
+document.querySelector(".answer__text").textContent = odpoved;
+
+const spravnaOdpoved = "Bell"
+
+const kontrola = document.querySelector(".answer");
+
+if (odpoved === spravnaOdpoved) {
+    kontrola.classList.add("answer--correct");
+} else {
+    kontrola.classList.add("answer--wrong");
+}
+
+
+// Vytvořte si repozitář ze šablony cviceni-kviz. V repozitáři je kostra stránky pro kvízové otázky. Napište JavaScriptový program, který vybere element s třídou question a nastaví jeho obsah na nějakou kvízovou otázku, například
+
+// Uveďte příjmení amerického vynálezce, který v roce 1876 jako první úspěšně podal patent na zařízení, které je dnes považováno za první telefon.
+
+// Použijte funkci prompt a zeptejte se uživatele na správnou odpověď. Vyberte element s třídou answer__text a nastavte obsah elementu na odpověď od uživatele. Vyberte element s třidou answer. Pokud je odpověď příjmení Bell, přidejte k tomuto elementu třídu answer--correct. V opačném případě přidejte třídu answer--wrong.
